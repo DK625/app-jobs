99962051ea02f190bfc2f6abf14f2d739561b1c6
csdn6969 <csdn2003@gmail.com>
2024-12-24 14:47:10 +0700

    tmp

--- fe-job-portal/src/components-sma/post/Post.jsx
+++ fe-job-portal/src/components-sma/post/Post.jsx
@@ -8,51 +8,49 @@ import {
   Comment,
 } from "@mui/icons-material";
 import { UserOutlined } from '@ant-design/icons';
-import { useContext, useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
-
 
 export default function Post({ post }) {
   const [like, setLike] = useState(post.like);
   const [isLiked, setIsLiked] = useState(false);
+
   const likeHandler = () => {
     setLike(isLiked ? like - 1 : like + 1);
     setIsLiked(!isLiked);
   };
 
-  const handleSavePost = async (e,postId) =>{
-    console.log(e)
-    console.log(postId)
-    try{
-      const res = await axios.post(`http://localhost:8000/api/posts/save/${postId}`,{
-        user_id: e
-      });
-      if (res.status === 200){
-        console.log('done')
-      }else{
-        console.log('loi')
+  // Hàm lưu bài viết
+  const handleSavePost = async (userId, postId) => {
+    console.log('User ID:', userId);  // Kiểm tra giá trị userId
+    console.log('Post ID:', postId);  // Kiểm tra giá trị postId
+    try {
+        const res = await axios.post(`http://localhost:8000/api/posts/save/${postId}`, {
+          "user_id": `${userId}`
+        });
+
+      console.log('res: ', res)
+      if (res.status === 200) {
+        console.log('Bài viết đã được lưu thành công!');
+      } else {
+        console.log('Lỗi khi lưu bài viết');
       }
-    }catch(err){
-      console.error('Loi khi goi Api', err)
+    } catch (err) {
+      console.error('Lỗi khi gọi API', err);
     }
-  }
+  };
 
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-           
-          <div className="avatarContainer">
-           <UserOutlined className="avatarIcon" />
-          </div>
-
+            <div className="avatarContainer">
+              <UserOutlined className="avatarIcon" />
+            </div>
             <div className="postProfileUsernameDate">
               <span className="postUsername">{post.author_id.fullName}</span>
-              {/*</Link>*/}
-              {/*<Link to={`/post/${post.id}`}>*/}
             </div>
-            {/*</Link>*/}
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -66,9 +64,9 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomApply">
-            <button class="button" style={{ fontSize: "16px" }}>
+            <button className="button" style={{ fontSize: "16px" }}>
               <Send />
-              <span class="buttonTextApply">Ứng tuyển ngay</span>
+              <span className="buttonTextApply">Ứng tuyển ngay</span>
             </button>
           </div>
           <hr className="postBottomHr" />
@@ -89,28 +87,29 @@ export default function Post({ post }) {
               <span className="postLikeCounter">{like}</span>
             </div>
             <div className="postBottomRight">
-              {/*<Link to={`/post/${post.id}`}>*/}
               <span className="postCommentText">{post.comment} bình luận</span>
-              {/*</Link>*/}
             </div>
           </div>
           <hr className="postBottomHr" />
           <div className="postBottomButtons">
-            <button class="button">
+            <button className="button">
               <ThumbUp />
-              <span class="buttonText">Thích</span>
+              <span className="buttonText">Thích</span>
             </button>
-            <button class="button">
+            <button className="button">
               <Comment />
-              <span class="buttonText">Bình luận</span>
+              <span className="buttonText">Bình luận</span>
             </button>
-            <button class="button">
+            <button
+              className="button"
+              onClick={() => handleSavePost(post.author_id._id, post._id)} // Truyền post.author_id._id và post._id
+            >
               <BookmarkAdd />
-              <span class="buttonText" onClick={() => handleSavePost(post.author_id._id, post._id)}>Lưu bài viết</span>
+              <span className="buttonText">Lưu bài viết</span>
             </button>
-            <button class="button">
+            <button className="button">
               <Share />
-              <span class="buttonText">Chia sẻ</span>
+              <span className="buttonText">Chia sẻ</span>
             </button>
           </div>
         </div>
